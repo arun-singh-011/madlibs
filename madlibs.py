a7820816e2cc1bfbd90ba08827d1aba657e1b914
ARUN SINGH <arunsinghh0011@gmail.com>
2025-02-01 23:16:37 -0800

    madlibs in python

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+youtuber =" arun"
+print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so happy all the time because I like to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
